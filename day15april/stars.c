11da430c1df2ea1db16b7e51dad3560c12fd9a2b
onlyvinit <vinitpanchal1147@gmail.com>
2024-04-15 17:47:48 +0530

    added

---
+++ day15april/stars.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5; 
+
+
+    for (int i = 0; i < rows; i++) {
+        
+        for (int j = 0; j < rows; j++) {
+            if (j >= i) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
