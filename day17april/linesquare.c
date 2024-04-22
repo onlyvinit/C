172e078d5187a4b6412dfbb891c2b39adba9e34c
onlyvinit <vinitpanchal1147@gmail.com>
2024-04-22 10:16:13 +0530

    added

---
+++ day17april/linesquare.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i <= 6; i++) {
+        for (int j = 0; j <= 6; j++) {
+            if (i == 0 || i == 6 || j == 0 || j == 6 || i == j) {
+                printf("* ");
+            } else {
+                printf("  "); 
+            }
+        }
+        printf("\n"); 
+    }
+    return 0;
+}
